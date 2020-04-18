278299b1b4c8de15d857b90a86782d9d4ca34a1f
Condition17 <c.conache@gmail.com>
2020-04-18 21:00:14 +0300

    Test commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import Application from "./components/Application";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Application from './components/Application';
 
 ReactDOM.render(
   <React.StrictMode>
     <Application />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
+
+// remove this
