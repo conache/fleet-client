0f0b250ab3f18ebc2e166a0f85c063e3938ad24f
Cristi <cristian@blink.network>
2020-07-27 23:29:07 +0300

    Small progress creating file uploader file

---
+++ src/components/upload/FileUploader.js
@@ -0,0 +1,7 @@
+class FileUploader {
+    constructor() {
+        console.log("File uploader was instantiated, hehehe");
+    }
+}
+
+export default FileUploader;
--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import FileUploader from './FileUploader';
 
 class Upload extends Component {
+    constructor() {
+        super();
+        
+        this.fileUploader = new FileUploader();
+    }
+
     render() {
         return <div> This is a test component</div>
     }
