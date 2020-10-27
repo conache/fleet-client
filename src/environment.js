bf92d63314681a3eeb47d581f602c0c3ee0254b1
conache <c.conache@gmail.com>
2020-10-27 22:24:08 +0200

    Trivial connection to the new wss service

--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,6 @@
+function isProdEnvironment() {
+  return process.env.REACT_APP_ENV === "prod"
+}
+
 export const __BASE_API_URL__ = 'http://104.154.76.66';
-export const __BASE_SOCKET_SERVER_URL__ = "http://35.202.147.65";
+export const __BASE_SOCKET_SERVER_URL__ = isProdEnvironment() ? "http://35.202.147.65" : "localhost:3001";
