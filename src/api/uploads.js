a1b2e3dab5e497fdd00697ea8167d2efb3c2025c
conache <c.conache@gmail.com>
2020-11-25 10:19:07 +0200

    Support latest backend changes

--- src/api/uploads.js
+++ src/api/uploads.js
@@ -1,9 +1,9 @@
 import {request, httpVerb } from './request'
 
 export function createUpload(fileSpecification={}) {
-  return request(httpVerb.POST, 'file-service/createFile', fileSpecification);
+  return request(httpVerb.POST, 'fileservice/FileService/CreateFile', fileSpecification);
 }
 
 export function sendFileChunk(body={}) {
-  return request(httpVerb.POST, 'file-service/createChunk', body);
+  return request(httpVerb.POST, 'fileservice/FileService/CreateChunk', body);
 }
--- src/environment.js
+++ src/environment.js
@@ -2,5 +2,5 @@ function isProdEnvironment() {
   return process.env.REACT_APP_ENV === "prod"
 }
 
-export const __BASE_API_URL__ = isProdEnvironment() ? 'http://104.154.76.66' : "http://localhost:8080";
-export const __BASE_SOCKET_SERVER_URL__ = isProdEnvironment() ? "http://35.202.147.65" : "localhost:3001";
+export const __BASE_API_URL__ = isProdEnvironment() ? 'http://35.193.21.247' : "http://localhost:8080";
+export const __BASE_SOCKET_SERVER_URL__ = isProdEnvironment() ? "http://34.123.84.62" : "localhost:3001";
